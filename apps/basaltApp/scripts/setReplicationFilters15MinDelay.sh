171c55db97f3cd3fe0a84b4d69cd0e8890fa2839
Victor Woeltjen <victor.woeltjen@nasa.gov>
2018-02-13 11:35:39 -0800

    Remove remaining replication scripts

--- apps/basaltApp/scripts/setReplicationFilters15MinDelay.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-/home/irg/tungsten/tungsten/tools/tpm update --hosts=boat,shore --repl-svc-applier-filters=delay,replicate --property=replicator.filter.delay.delay=900 --property=replicator.filter.replicate.ignore=xgds_basalt.xgds_video_videosegment,xgds_basalt.xgds_video_videoepisode,xgds_basalt.xgds_core_constant,xgds_basalt.django_migrations,xgds_basalt.django_session,xgds_status_board_subsystem,xgds_status_board_subsystemgroup,xgds_basalt.xgds_core_relayevent,xgds_basalt.xgds_core_relayfile,xgds_basalt.basaltApp_basaltactiveflight
-
-ssh boat ./tungsten/tungsten/tools/tpm update --hosts=boat,shore --repl-svc-applier-filters=delay,replicate --property=replicator.filter.delay.delay=900 --property=replicator.filter.replicate.ignore=xgds_basalt.xgds_video_videosegment,xgds_basalt.xgds_video_videoepisode,xgds_basalt.xgds_core_constant,xgds_basalt.django_migrations,xgds_basalt.django_session,xgds_status_board_subsystem,xgds_status_board_subsystemgroup,xgds_basalt.xgds_core_relayevent,xgds_basalt.xgds_core_relayfile,xgds_basalt.basaltApp_basaltactiveflight
