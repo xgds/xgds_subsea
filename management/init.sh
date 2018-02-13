844dfa917a99136b6577fca03f916440a8821ace
Victor Woeltjen <victor.woeltjen@nasa.gov>
2018-02-13 13:21:53 -0800

    Sketch in commands

--- management/init.sh
+++ management/init.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 DIR=$(dirname $0)
-BASE_TEMPLATE=basaltApp
+FROM=basaltApp
+TO=${1:-xgds_app}
 APPS_DIR=$DIR/../apps
 TEMPLATES_DIR=$DIR/templates
 
-echo $1
-echo $APPS_DIR/$1
+echo "cp -r $TEMPLATES_DIR/$FROM $APPS_DIR/$TO"
 
+# cp -r $TEMPLATES_DIR/$BASE_TEMPLATE $APPS_DIR
