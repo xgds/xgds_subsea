f490948b60d36f7c540cadc8510951baed3aef01
Victor Woeltjen <victor.woeltjen@nasa.gov>
2018-02-28 14:34:14 -0800

    genericize directory name

--- apps/xgds_baseline_app/urls.py
+++ apps/xgds_baseline_app/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls import include, url
 from xgds_core.register import renderTemplate
 import restUrls
 
-urlpatterns = [url(r'^$', renderTemplate, {'template_name':'xgds_baseline_app/index.html'}, 'index'),
+urlpatterns = [url(r'^$', renderTemplate, {'template_name':'xgds_app/index.html'}, 'index'),
                # Including these in this order ensures that reverse will return the non-rest urls for use in our server
                url(r'^rest/', include(restUrls)),
                url('', include(restUrls)),
