b2f66195a101351bb163bbf790cef7c9105b1739
Tamar Cohen <tamar.cohen@nasa.gov>
2018-05-15 08:44:43 -0700

    included imports in models.py

--- apps/xgds_baseline_app/models.py
+++ apps/xgds_baseline_app/models.py
@@ -13,3 +13,6 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 #__END_LICENSE__
+from __future__ import unicode_literals
+
+from django.db import models
