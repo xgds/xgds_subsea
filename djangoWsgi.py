addcd8574fc219086d7f1cad9bd9e5e6189aed23
Tamar Cohen <tamar.cohen@nasa.gov>
2019-02-27 17:47:06 -0800

    fixed image annotations and added new djangoWsgi that logs headers optionally

--- djangoWsgi.py
+++ djangoWsgi.py
@@ -22,6 +22,10 @@ import re
 # avoid crazy error on mac os
 os.environ['PYTHON_EGG_CACHE'] = '/tmp'
 
+# turn this on to log all headers to /tmp/wsgi
+LOG_HEADERS = False
+
+
 def getEnvironmentFromSourceMe(d='.'):
     # pick up environment variables from sourceme
     fd, varsFile = tempfile.mkstemp('djangoWsgiSourceMe.txt')
@@ -86,9 +90,112 @@ from xgds_core.tokenAuth import check_password
 
 # end apache login
 
+# Logging WSGI middleware.
+
+import threading
+import pprint
+import time
+import os
+
+
+class LoggingInstance:
+    def __init__(self, start_response, oheaders, ocontent):
+        self.__start_response = start_response
+        self.__oheaders = oheaders
+        self.__ocontent = ocontent
+
+    def __call__(self, status, headers, *args):
+        printable = (status, headers)+args
+        pprint.pprint(printable, stream=self.__oheaders)
+        self.__oheaders.close()
+
+        self.__write = self.__start_response(status, headers, *args)
+        return self.write
+
+    def __iter__(self):
+        return self
+
+    def write(self, data):
+        self.__ocontent.write(data)
+        self.__ocontent.flush()
+        return self.__write(data)
+
+    def next(self):
+        data = self.__iterable.next()
+        self.__ocontent.write(data)
+        self.__ocontent.flush()
+        return data
+
+    def close(self):
+        if hasattr(self.__iterable, 'close'):
+            self.__iterable.close()
+        self.__ocontent.close()
+
+    def link(self, iterable):
+        self.__iterable = iter(iterable)
+
+
+class LoggingMiddleware:
+
+    def __init__(self, application, savedir):
+        self.__application = application
+        self.__savedir = savedir
+        self.__lock = threading.Lock()
+        self.__pid = os.getpid()
+        self.__count = 0
+
+    def __call__(self, environ, start_response):
+        self.__lock.acquire()
+        self.__count += 1
+        count = self.__count
+        self.__lock.release()
+
+        key = "%s-%s-%s" % (time.time(), self.__pid, count)
+
+        iheaders = os.path.join(self.__savedir, key + ".iheaders")
+        iheaders_fp = file(iheaders, 'w')
+
+        icontent = os.path.join(self.__savedir, key + ".icontent")
+        icontent_fp = file(icontent, 'w+b')
+
+        oheaders = os.path.join(self.__savedir, key + ".oheaders")
+        oheaders_fp = file(oheaders, 'w')
+
+        ocontent = os.path.join(self.__savedir, key + ".ocontent")
+        ocontent_fp = file(ocontent, 'w+b')
+
+        errors = environ['wsgi.errors']
+        pprint.pprint(environ, stream=iheaders_fp)
+        iheaders_fp.close()
+
+        length = int(environ.get('CONTENT_LENGTH', '0'))
+        input = environ['wsgi.input']
+        while length != 0:
+            data = input.read(min(4096, length))
+            if data:
+                icontent_fp.write(data)
+                length -= len(data)
+            else:
+                length = 0
+        icontent_fp.flush()
+        icontent_fp.seek(0, os.SEEK_SET)
+        environ['wsgi.input'] = icontent_fp
+
+        iterable = LoggingInstance(start_response, oheaders_fp, ocontent_fp)
+        iterable.link(self.__application(environ, iterable))
+        return iterable
+
+
 if os.path.exists(os.path.join(thisDir, 'DOWN_FOR_MAINTENANCE')):
     application = downForMaintenance
 else:
     #os.environ['DJANGO_SETTINGS_MODULE'] = 'basaltApp.settings'
     from django.core.wsgi import get_wsgi_application
-    application = get_wsgi_application()
+    if LOG_HEADERS:
+        logdir = '/tmp/wsgi'
+        if not os.path.exists(logdir):
+            os.makedirs(logdir)
+        application = LoggingMiddleware(get_wsgi_application(), logdir)
+    else:
+        application = get_wsgi_application()
+
