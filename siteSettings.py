ecf1a39578b1259634a99fbe27e69f4300772e97
Tamar Cohen <tamar.cohen@nasa.gov>
2019-03-13 13:24:09 -0700

    added special characters pw validation and better hashing

--- siteSettings.py
+++ siteSettings.py
@@ -561,3 +561,30 @@ if 'Science Instruments' in XGDS_DATA_IMPORTS:
 
 
 XGDS_VIDEO_DEFAULT_AUDIO_SOURCE = "Hercules"
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'xgds_core.password_validation.SpecialCharsValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    }
+
+]
