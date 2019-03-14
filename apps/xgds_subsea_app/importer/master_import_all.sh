39bc9126d414c94607686a98dc8561032a9e7850
Tamar Cohen <tamar.cohen@nasa.gov>
2019-03-14 14:52:21 -0700

    updated planner and added master import all script

---
+++ apps/xgds_subsea_app/importer/master_import_all.sh
@@ -0,0 +1,22 @@
+
+# This script does all the steps to preprocess and then do an import
+# you must be in
+
+# configure directories
+BASE_DIR=/root/xgds_subsea
+DATA_DIR=$BASE_DIR/data
+CRUISE_ID=NA100
+IMPORT_CONFIG=$BASE_DIR/apps/xgds_subsea_app/importer/ImportHandlerConfig.yaml
+
+source $BASE_DIR/sourceme.sh
+
+# preprocess nav data
+echo "*** PREPROCESSING NAV DATA FROM $DATA_DIR/$CRUISE_ID"
+$BASE_DIR/apps/xgds_subsea_app/importer/preprocess_nav_data.py $DATA_DIR/$CRUISE_ID
+echo "*** DONE PREPROCESSING NAV DATA FROM $DATA_DIR/$CRUISE_ID"
+
+# do the actual import
+echo "*** CALLING IMPORTER FROM $IMPORT_CONFIG"
+$BASE_DIR/apps/xgds_core/importer/importHandler.py --user xgds --password TODO_CHANGEME $IMPORT_CONFIG
+echo "*** DONE CALLING IMPORTER FROM $IMPORT_CONFIG"
+
